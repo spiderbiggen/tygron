4a386c8f3639134c40a62bcf7676a7c3c6d6a7f0
Wouter1 <W.Pasman@TUDelft.nl>
2016-02-11 09:29:12 +0100

    #3781 moved test configuration to test directory

--- tygron-connect-api/src/test/java/nl/tudelft/contextproject/tygron/SettingsLoaderTest.java
+++ tygron-connect-api/src/test/java/nl/tudelft/contextproject/tygron/SettingsLoaderTest.java
@@ -3,35 +3,32 @@ package nl.tudelft.contextproject.tygron;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import nl.tudelft.contextproject.tygron.SettingsLoader;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class SettingsLoaderTest {
-  SettingsLoader settingsLoader;
+	SettingsLoader settingsLoader;
 
-  /**
-   * Initialize test.
-   */
-  @Before
-  public void start() {
-    try {
-      settingsLoader = new SettingsLoader(
-          "src/main/resources/testconfiguration.cfg");
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail("File not found or could not be read");
-    }
-  }
+	/**
+	 * Initialize test.
+	 */
+	@Before
+	public void start() {
+		try {
+			settingsLoader = new SettingsLoader("src/test/resources/testconfiguration.cfg");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("File not found or could not be read");
+		}
+	}
 
-  @Test
-  public void test_username() {
-    assertTrue(settingsLoader.getUsername().equals("demousername"));
-  }
+	@Test
+	public void test_username() {
+		assertTrue(settingsLoader.getUsername().equals("demousername"));
+	}
 
-  @Test
-  public void test_password() {
-    assertTrue(settingsLoader.getPassword().equals("demopassword"));
-  }
+	@Test
+	public void test_password() {
+		assertTrue(settingsLoader.getPassword().equals("demopassword"));
+	}
 }
