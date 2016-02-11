109d4862e6f65d0295cdfbea91e32ba5c017219b
Wouter1 <W.Pasman@TUDelft.nl>
2016-02-11 13:31:36 +0100

    #3781 fixed default headers to work around "unsupported media type" errors from the server

--- tygron-connect-api/src/test/java/nl/tudelft/contextproject/tygron/api/ConnectorTest.java
+++ tygron-connect-api/src/test/java/nl/tudelft/contextproject/tygron/api/ConnectorTest.java
@@ -1,8 +1,5 @@
 package nl.tudelft.contextproject.tygron.api;
 
-import nl.tudelft.contextproject.tygron.CachedFileReader;
-import nl.tudelft.contextproject.tygron.handlers.JsonObjectResultHandler;
-
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,39 +11,39 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import nl.tudelft.contextproject.tygron.CachedFileReader;
+import nl.tudelft.contextproject.tygron.handlers.JsonObjectResultHandler;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(HttpConnection.class)
 public class ConnectorTest {
 
-  @Mock
-  HttpConnection connection;
-  
-  Connector connector;
-  
-  /**
-   * Test the connector.
-   */
-  @Before
-  public void createConnector() {
-    
-    PowerMockito.mockStatic(HttpConnection.class);
-    BDDMockito.given(HttpConnection.getInstance()).willReturn(connection);
-    
-    String myUserString = CachedFileReader.getFileContents("/serverResponses/myUser.json");
-    
-    System.out.println(myUserString);
-    Mockito.when(
-        connection.execute(Mockito.eq("services/myuser"), Mockito.eq(CallType.GET),
-            Mockito.any(JsonObjectResultHandler.class)
-            )).thenReturn(
-                new JSONObject(myUserString));
-    
-    connector = new Connector();
-    
-  }
-  
-  @Test
-  public void userLoadTest() {
-    connector.loadUserData();
-  }
+	@Mock
+	HttpConnection connection;
+
+	Connector connector;
+
+	/**
+	 * Test the connector.
+	 */
+	@Before
+	public void createConnector() {
+
+		PowerMockito.mockStatic(HttpConnection.class);
+		BDDMockito.given(HttpConnection.getInstance()).willReturn(connection);
+
+		String myUserString = CachedFileReader.getFileContents("/serverResponses/myUser.json");
+
+		System.out.println(myUserString);
+		Mockito.when(connection.execute(Mockito.eq("services/myuser"), Mockito.eq(CallType.GET),
+				Mockito.any(JsonObjectResultHandler.class))).thenReturn(new JSONObject(myUserString));
+
+		connector = new Connector();
+
+	}
+
+	@Test
+	public void userLoadTest() {
+		connector.loadUserData();
+	}
 }
