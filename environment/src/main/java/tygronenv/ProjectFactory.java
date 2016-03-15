08ded73a125f7df6f094065686a51d8c0d68a8ac
Wouter1 <W.Pasman@TUDelft.nl>
2016-03-15 10:08:54 +0100

    #3803 moved project init code to ProjectFactory

---
+++ environment/src/main/java/tygronenv/ProjectFactory.java
@@ -0,0 +1,152 @@
+package tygronenv;
+
+import eis.exceptions.ManagementException;
+import nl.tytech.core.client.net.ServicesManager;
+import nl.tytech.core.client.net.SlotConnection;
+import nl.tytech.core.net.Network.AppType;
+import nl.tytech.core.net.Network.SessionType;
+import nl.tytech.core.net.event.IOServiceEventType;
+import nl.tytech.core.net.serializable.JoinReply;
+import nl.tytech.core.net.serializable.ProjectData;
+import nl.tytech.core.util.SettingsManager;
+import nl.tytech.data.editor.event.EditorEventType;
+import nl.tytech.data.editor.event.EditorSettingsEventType;
+import nl.tytech.data.editor.event.EditorStakeholderEventType;
+import nl.tytech.data.engine.item.Stakeholder;
+import nl.tytech.locale.TLanguage;
+import nl.tytech.util.ThreadUtils;
+
+/**
+ * Factory to fetch existing and create new projects
+ * 
+ * @author W.Pasman
+ *
+ */
+public class ProjectFactory {
+
+	/**
+	 * Join existing project.
+	 * 
+	 * @param name
+	 *            the project name to join/make.
+	 * @return project with given name, or null if no project with given name
+	 *         exists.
+	 * @throws ManagementException
+	 */
+	public ProjectData getProject(String name) throws ManagementException {
+		ProjectData[] projects = ServicesManager.fireServiceEvent(IOServiceEventType.GET_MY_STARTABLE_PROJECTS);
+		if (projects != null) {
+			for (ProjectData existing : projects) {
+				if (existing.getFileName().equals(name)) {
+					return ServicesManager.fireServiceEvent(IOServiceEventType.GET_PROJECT_DATA, name);
+				}
+			}
+		}
+
+		return createProject(name);
+	}
+
+	/**
+	 * There is no existing project with given name. Create one and initialize
+	 * it. Bit hacky, it does not do much for the map, I assume it stays empty.
+	 * 
+	 * @throws ManagementException
+	 */
+	public ProjectData createProject(String name) throws ManagementException {
+		ProjectData proj = ServicesManager.fireServiceEvent(IOServiceEventType.CREATE_NEW_PROJECT, name, TLanguage.EN);
+
+		Integer slotID = ServicesManager.fireServiceEvent(IOServiceEventType.START_NEW_SESSION, SessionType.EDITOR,
+				proj.getFileName(), TLanguage.EN);
+		if (slotID == null || slotID < 0) {
+			throw new ManagementException("Failed to create edit slot to create new project");
+		}
+
+		SlotConnection editSlot = editProject(slotID);
+		addCivilianMap(editSlot);
+
+		String result = ServicesManager.fireServiceEvent(IOServiceEventType.SAVE_PROJECT_INIT, slotID);
+		if (result != null) {
+			throw new ManagementException("Failed to save new project" + result);
+		}
+
+		/**
+		 * Disconnect from slot
+		 */
+		editSlot.disconnect(false);
+
+		return proj;
+	}
+
+	/**
+	 * @param proj
+	 *            the project to open an edit slot for
+	 * @return a {@link SlotConnection} that can be used for editing the
+	 *         project.
+	 * @throws ManagementException
+	 */
+	private SlotConnection editProject(Integer slotID) throws ManagementException {
+
+		JoinReply reply = ServicesManager.fireServiceEvent(IOServiceEventType.JOIN_SESSION, slotID, AppType.EDITOR);
+		if (reply == null) {
+			throw new ManagementException("failed to edit project:" + reply);
+		}
+
+		SlotConnection slotConnection = new SlotConnection();
+		slotConnection.initSettings(AppType.EDITOR, SettingsManager.getServerIP(), slotID, reply.serverToken,
+				reply.client.getClientToken());
+		if (!slotConnection.connect()) {
+			throw new ManagementException("Failed to connect a slot for editing the new project");
+		}
+
+		return slotConnection;
+	}
+
+	/**
+	 * Add map and civilian stakeholder
+	 * 
+	 * @param slotConnection
+	 *            the connection with the editor slot
+	 * @throws ManagementException
+	 */
+	private void addCivilianMap(SlotConnection slotConnection) throws ManagementException {
+		// add event handler to receive updates on
+		EditorEventHandler eventHandler = new EditorEventHandler();
+		int mapSizeM = 500;
+		slotConnection.fireServerEvent(true, EditorEventType.SET_INITIAL_MAP_SIZE, mapSizeM);
+		slotConnection.fireServerEvent(true, EditorSettingsEventType.WIZARD_FINISHED);
+
+		/**
+		 * Add a civilian stakeholder
+		 */
+		slotConnection.fireServerEvent(true, EditorStakeholderEventType.ADD_WITH_TYPE_AND_PLAYABLE,
+				Stakeholder.Type.CIVILIAN, true);
+
+		// wait on first updates (seperate thread)
+		boolean updated = false;
+		for (int i = 0; i < 15; i++) {
+			if (eventHandler.isMapUpdated() && eventHandler.isStakeholderUpdated()) {
+				updated = true;
+				break;
+			}
+			ThreadUtils.sleepInterruptible(1000);
+		}
+		if (!updated) {
+			throw new ManagementException("Server is not responding on request to update the map");
+		}
+	}
+
+	/**
+	 * Deletes project on the server.
+	 * 
+	 * @param project
+	 * @throws ManagementException
+	 */
+	public void deleteProject(ProjectData project) throws ManagementException {
+		Boolean result = ServicesManager.fireServiceEvent(IOServiceEventType.DELETE_PROJECT, project.getFileName());
+		if (!result) {
+			throw new ManagementException("failed to delete project " + project.getFileName() + " on the server");
+		}
+
+	}
+
+}
--- environment/src/main/java/tygronenv/ServerConnection.java
+++ environment/src/main/java/tygronenv/ServerConnection.java
@@ -6,26 +6,16 @@ import eis.exceptions.ManagementException;
 import nl.tytech.core.client.event.EventIDListenerInterface;
 import nl.tytech.core.client.event.EventManager;
 import nl.tytech.core.client.net.ServicesManager;
-import nl.tytech.core.client.net.SlotConnection;
 import nl.tytech.core.event.Event;
 import nl.tytech.core.event.EventListenerInterface;
 import nl.tytech.core.net.Network;
-import nl.tytech.core.net.Network.AppType;
-import nl.tytech.core.net.Network.SessionType;
-import nl.tytech.core.net.event.IOServiceEventType;
-import nl.tytech.core.net.serializable.JoinReply;
 import nl.tytech.core.net.serializable.MapLink;
 import nl.tytech.core.net.serializable.ProjectData;
 import nl.tytech.core.net.serializable.User;
 import nl.tytech.core.net.serializable.User.AccessLevel;
 import nl.tytech.core.util.SettingsManager;
-import nl.tytech.data.editor.event.EditorEventType;
-import nl.tytech.data.editor.event.EditorSettingsEventType;
-import nl.tytech.data.editor.event.EditorStakeholderEventType;
 import nl.tytech.data.engine.item.Setting;
 import nl.tytech.data.engine.item.Stakeholder;
-import nl.tytech.locale.TLanguage;
-import nl.tytech.util.ThreadUtils;
 import nl.tytech.util.logger.TLogger;
 import tygronenv.settings.Settings;
 
@@ -45,6 +35,8 @@ public class ServerConnection {
 	 */
 	private boolean createdProject = false;
 	private ProjectData project;
+
+	private ProjectFactory factory = new ProjectFactory();
 
 	/**
 	 * Connect with the server, using the {@link Settings}.
@@ -77,134 +69,27 @@ public class ServerConnection {
 			throw new ManagementException("You need to have at least EDITOR access level");
 		}
 
-		project = getProject(config.getMap());
+		project = factory.getProject(config.getMap());
+		if (project == null) {
+			project = factory.createProject(config.getMap());
+			createdProject = true;
+		}
+
 		session = new SessionManager(config, project);
-	}
-
-	/**
-	 * Join existing or create new project.
-	 * 
-	 * @param name
-	 *            the project name to join/make.
-	 * @return project with given name
-	 * @throws ManagementException
-	 */
-	private ProjectData getProject(String name) throws ManagementException {
-		ProjectData[] projects = ServicesManager.fireServiceEvent(IOServiceEventType.GET_MY_STARTABLE_PROJECTS);
-		if (projects != null) {
-			for (ProjectData existing : projects) {
-				if (existing.getFileName().equals(name)) {
-					return ServicesManager.fireServiceEvent(IOServiceEventType.GET_PROJECT_DATA, name);
-				}
-			}
-		}
-
-		return createProject(name);
-	}
-
-	/**
-	 * There is no existing project with given name. Create one and initialize
-	 * it.
-	 * 
-	 * @throws ManagementException
-	 */
-	private ProjectData createProject(String name) throws ManagementException {
-		ProjectData proj = ServicesManager.fireServiceEvent(IOServiceEventType.CREATE_NEW_PROJECT, name, TLanguage.EN);
-
-		Integer slotID = ServicesManager.fireServiceEvent(IOServiceEventType.START_NEW_SESSION, SessionType.EDITOR,
-				proj.getFileName(), TLanguage.EN);
-		if (slotID == null || slotID < 0) {
-			throw new ManagementException("Failed to create edit slot to create new project");
-		}
-
-		SlotConnection editSlot = editProject(slotID);
-		addCivilianMap(editSlot);
-
-		String result = ServicesManager.fireServiceEvent(IOServiceEventType.SAVE_PROJECT_INIT, slotID);
-		if (result != null) {
-			throw new ManagementException("Failed to save new project" + result);
-		}
-
-		/**
-		 * Disconnect from slot
-		 */
-		editSlot.disconnect(false);
-
-		createdProject = true;
-		return proj;
-	}
-
-	/**
-	 * @param proj
-	 *            the project to open an edit slot for
-	 * @return a {@link SlotConnection} that can be used for editing the
-	 *         project.
-	 * @throws ManagementException
-	 */
-	private SlotConnection editProject(Integer slotID) throws ManagementException {
-
-		JoinReply reply = ServicesManager.fireServiceEvent(IOServiceEventType.JOIN_SESSION, slotID, AppType.EDITOR);
-		if (reply == null) {
-			throw new ManagementException("failed to edit project " + project + ":" + reply);
-		}
-
-		SlotConnection slotConnection = new SlotConnection();
-		slotConnection.initSettings(AppType.EDITOR, SettingsManager.getServerIP(), slotID, reply.serverToken,
-				reply.client.getClientToken());
-		if (!slotConnection.connect()) {
-			throw new ManagementException("Failed to connect a slot for editing the new project");
-		}
-
-		return slotConnection;
-	}
-
-	/**
-	 * Add map and civilian stakeholder
-	 * 
-	 * @param slotConnection
-	 *            the connection with the editor slot
-	 * @throws ManagementException
-	 */
-	private void addCivilianMap(SlotConnection slotConnection) throws ManagementException {
-		// add event handler to receive updates on
-		EditorEventHandler eventHandler = new EditorEventHandler();
-		int mapSizeM = 500;
-		slotConnection.fireServerEvent(true, EditorEventType.SET_INITIAL_MAP_SIZE, mapSizeM);
-		slotConnection.fireServerEvent(true, EditorSettingsEventType.WIZARD_FINISHED);
-
-		/**
-		 * Add a civilian stakeholder
-		 */
-		slotConnection.fireServerEvent(true, EditorStakeholderEventType.ADD_WITH_TYPE_AND_PLAYABLE,
-				Stakeholder.Type.CIVILIAN, true);
-
-		// wait on first updates (seperate thread)
-		boolean updated = false;
-		for (int i = 0; i < 15; i++) {
-			if (eventHandler.isMapUpdated() && eventHandler.isStakeholderUpdated()) {
-				updated = true;
-				break;
-			}
-			ThreadUtils.sleepInterruptible(1000);
-		}
-		if (!updated) {
-			throw new ManagementException("Server is not responding on request to update the map");
-		}
 	}
 
 	/**
 	 * Disconnect from server. After calling this, this object can not be used
 	 * anymore.
+	 * 
+	 * @throws ManagementException
 	 */
-	public void disconnect() {
+	public void disconnect() throws ManagementException {
 		if (session != null) {
 			session.close();
 		}
 		if (createdProject) {
-			Boolean result = ServicesManager.fireServiceEvent(IOServiceEventType.DELETE_PROJECT, project.getFileName());
-			if (!result) {
-				System.out.println("WARNING: failed to delete project " + project.getFileName() + " on the server");
-			}
+			factory.deleteProject(project);
 			project = null;
 			createdProject = false;
 		}
