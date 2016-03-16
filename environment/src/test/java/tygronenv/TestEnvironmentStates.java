51002f1dd0d8c61b06150cf603aa9b469f5ac50a
Wouter1 <W.Pasman@TUDelft.nl>
2016-03-16 13:16:25 +0100

    #3803 changed the test, to ensure we subscribe listener immediately when the free entity is reported.

--- environment/src/test/java/tygronenv/TestEnvironmentStates.java
+++ environment/src/test/java/tygronenv/TestEnvironmentStates.java
@@ -1,8 +1,10 @@
 package tygronenv;
 
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +16,7 @@ import eis.EnvironmentListener;
 import eis.exceptions.AgentException;
 import eis.exceptions.ManagementException;
 import eis.exceptions.RelationException;
+import eis.iilang.EnvironmentState;
 import eis.iilang.Identifier;
 import eis.iilang.Parameter;
 import eis.iilang.ParameterList;
@@ -33,7 +36,7 @@ public class TestEnvironmentStates {
 	private final String ENTITY = "entity";
 
 	@Test
-	public void testStateChange() throws ManagementException, RelationException, AgentException {
+	public void testEntityAppears() throws ManagementException, RelationException, AgentException {
 
 		EnvironmentListener envlistener = mock(EnvironmentListener.class);
 		AgentListener agentlistener = mock(AgentListener.class);
@@ -48,23 +51,82 @@ public class TestEnvironmentStates {
 		// after the init, a new entity should appear that we can connect to.
 		verify(envlistener).handleNewEntity(ENTITY);
 
-		// try to connect
-		env.registerAgent(AGENT);
-		env.associateEntity(AGENT, ENTITY);
-		env.attachAgentListener(AGENT, agentlistener);
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// check that agent listener receives initial percepts.
-		Percept expectedPercept = new Percept("stakeholders",
-				new ParameterList(new Parameter[] { new Identifier("Municipality"), new Identifier("Inhabitants") }));
-		verify(agentlistener).handlePercept(AGENT, expectedPercept);
 		env.kill();
 	}
+
+	@Test
+	public void testStateChange() throws ManagementException, RelationException, AgentException, InterruptedException {
+		// try to connect
+		AgentListener agentlistener = mock(AgentListener.class);
+
+		MyEnvListener envlistener = new MyEnvListener(agentlistener);
+
+		env.attachEnvironmentListener(envlistener);
+		Map<String, Parameter> parameters = new HashMap<String, Parameter>();
+		parameters.put("map", MAP);
+		parameters.put("stakeholder", new Identifier("MUNICIPALITY"));
+		// any slot so not specified.
+		env.init(parameters);
+
+		while (!envlistener.connected) {
+			Thread.sleep(100);
+		}
+		assertNull(envlistener.exitcondition); // check connect went ok.
+
+		Thread.sleep(3000); // wait some more, to receive expected percept.
+
+		// check that agent listener receives initial percepts.
+		// there later will be another stakeholders percept with , new
+		// Identifier("Inhabitants")
+		Percept expectedPercept = new Percept("stakeholders",
+				new ParameterList(new Parameter[] { new Identifier("Municipality") }));
+		verify(agentlistener).handlePercept(AGENT, expectedPercept);
+
+		env.kill();
+	}
+
+	/**
+	 * Custom listener, so that we can immediately attach the agent when the
+	 * entity appears. This makes sure that we do not miss percepts.
+	 *
+	 */
+	class MyEnvListener implements EnvironmentListener {
+		public boolean connected = false;
+		public Exception exitcondition = null;
+		private AgentListener agentlistener;
+
+		public MyEnvListener(AgentListener agentlistener) {
+			this.agentlistener = agentlistener;
+		}
+
+		@Override
+		public void handleStateChange(EnvironmentState newState) {
+		}
+
+		@Override
+		public void handleNewEntity(String entity) {
+			try {
+				connectAgent(entity);
+			} catch (Exception e) {
+				exitcondition = e;
+			}
+			connected = true;
+		}
+
+		@Override
+		public void handleFreeEntity(String entity, Collection<String> agents) {
+		}
+
+		@Override
+		public void handleDeletedEntity(String entity, Collection<String> agents) {
+		}
+
+		private void connectAgent(String entity) throws AgentException, RelationException {
+			env.registerAgent(AGENT);
+			env.associateEntity(AGENT, ENTITY);
+			env.attachAgentListener(AGENT, agentlistener);
+		}
+
+	};
 
 }
