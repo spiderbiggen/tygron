9ef4fb7dfe6ea631de6dafdec45e6c136206291f
Wouter1 <W.Pasman@TUDelft.nl>
2016-03-16 10:13:17 +0100

    #3803 modified event handlers

--- environment/src/main/java/tygronenv/EisEnv.java
+++ environment/src/main/java/tygronenv/EisEnv.java
@@ -19,6 +19,7 @@ import tygronenv.configuration.Configuration;
 import tygronenv.connection.ServerConnection;
 import tygronenv.translators.HashMap2J;
 import tygronenv.translators.J2ClientItemMap;
+import tygronenv.translators.J2Setting;
 import tygronenv.translators.J2Stakeholder;
 import tygronenv.translators.ParamEnum2J;
 import tygronenv.translators.Stakeholder2J;
@@ -112,7 +113,7 @@ public class EisEnv extends EIDefaultImpl {
 
 	/************************* SUPPORT FUNCTIONS ****************************/
 
-	Java2Parameter<?>[] j2p = new Java2Parameter<?>[] { new J2ClientItemMap(), new J2Stakeholder() };
+	Java2Parameter<?>[] j2p = new Java2Parameter<?>[] { new J2ClientItemMap(), new J2Stakeholder(), new J2Setting() };
 	Parameter2Java<?>[] p2j = new Parameter2Java<?>[] { new ParamEnum2J(), new HashMap2J(), new Stakeholder2J() };
 
 	/**
--- environment/src/main/java/tygronenv/EntityEventHandler.java
+++ environment/src/main/java/tygronenv/EntityEventHandler.java
@@ -1,16 +1,18 @@
 package tygronenv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Translator;
 import eis.iilang.Parameter;
-import eis.iilang.ParameterList;
 import eis.iilang.Percept;
 import nl.tytech.core.client.event.EventIDListenerInterface;
 import nl.tytech.core.client.event.EventManager;
 import nl.tytech.core.event.Event;
+import nl.tytech.core.event.Event.EventTypeEnum;
 import nl.tytech.core.event.EventListenerInterface;
 import nl.tytech.core.net.serializable.MapLink;
-import nl.tytech.core.structure.ClientItemMap;
 import nl.tytech.core.structure.ItemMap;
 import nl.tytech.data.core.item.Item;
 import nl.tytech.data.engine.item.Setting;
@@ -28,13 +30,27 @@ public class EntityEventHandler implements EventListenerInterface, EventIDListen
 
 	public EntityEventHandler(PerceptPipe perceptPipe) {
 		pipe = perceptPipe;
-		EventManager.addListener(this, MapLink.STAKEHOLDERS);// MapLink.FUNCTIONS
+		EventManager.addListener(this, MapLink.STAKEHOLDERS, MapLink.FUNCTIONS);
 		EventManager.addEnumListener(this, MapLink.SETTINGS, Setting.Type.MAP_WIDTH_METERS);
 	}
 
 	@Override
 	public void notifyEnumListener(Event event, Enum<?> enhum) {
 		System.out.println("ENUM EVENT:" + event);
+		EventTypeEnum type = event.getType();
+		Object[] contents = event.getContents();
+		List<Percept> percepts = null;
+		switch (type.name()) {
+		case "SETTINGS":
+			percepts = createPercepts((List<?>) contents[1], type);
+			break;
+		default:
+			System.out.println("WARNING. EntityEventHandler ENUM received unknown event:" + event);
+			return;
+
+		}
+		pushAll(percepts);
+
 	}
 
 	@Override
@@ -44,22 +60,65 @@ public class EntityEventHandler implements EventListenerInterface, EventIDListen
 
 	@Override
 	public void notifyListener(Event event) {
-		if (event.getContent() instanceof ClientItemMap) {
-			Parameter[] parameters = null;
+		EventTypeEnum type = event.getType();
+		Object[] contents = event.getContents();
+		List<Percept> percepts;
+
+		switch (type.name()) {
+		case "STAKEHOLDERS":
+			percepts = createPercepts((List<?>) contents[1], type);
+			break;
+		case "FUNCTIONS":
+			percepts = createPercepts((List<?>) contents[1], type);
+			break;
+		default:
+			System.out.println("WARNING. EntityEventHandler received unknown event:" + event);
+			return;
+
+		}
+
+		pushAll(percepts);
+
+	}
+
+	/**
+	 * Push all percepts into the percepts pipe.
+	 * 
+	 * @param percepts
+	 *            list of percepts. Ignored if null.
+	 */
+	void pushAll(List<Percept> percepts) {
+		if (percepts == null)
+			return;
+		for (Percept percept : percepts) {
 			try {
-				// CHECK: event can have multiple contents?
-				if (event.getContents().length != 2) {
-					System.out.println("WARNING! EntityEventHandler: >2 content in a ClientItemMap");
-				}
-				parameters = translator.translate2Parameter(event.getContent());
-			} catch (TranslationException e) {
+				pipe.push(percept);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			if (parameters != null) {
-				Percept percept = new Percept(event.getType().name().toLowerCase(), new ParameterList(parameters));
-				pipe.push(percept);
-			}
 		}
+	}
+
+	/**
+	 * Create percepts contained in a ClientItemMap array
+	 * 
+	 * @param map
+	 *            list of ClientItemMap elements.
+	 * @param type
+	 *            the type of elements in the map.
+	 */
+	private List<Percept> createPercepts(List<?> list, EventTypeEnum type) {
+		List<Percept> percepts = new ArrayList<Percept>();
+		Parameter[] parameters = null;
+		try {
+			parameters = translator.translate2Parameter(list);
+		} catch (TranslationException e) {
+			e.printStackTrace();
+		}
+		if (parameters != null) {
+			percepts.add(new Percept(type.name().toLowerCase(), parameters));
+		}
+		return percepts;
 	}
 
 	public void stop() {
