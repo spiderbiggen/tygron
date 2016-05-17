e1ef83dc1f514bc9b341936be7f42584d4511716
hmyeh <h.m.yeh@student.tudelft.nl>
2016-05-17 18:11:02 +0200

    Changed stakeholder percept to indicatorlink percept

--- environment/src/main/java/tygronenv/translators/J2Stakeholder.java
+++ environment/src/main/java/tygronenv/translators/J2Stakeholder.java
@@ -1,16 +1,25 @@
 package tygronenv.translators;
 
+import java.util.List;
+
 import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Java2Parameter;
+import eis.iilang.Function;
 import eis.iilang.Identifier;
+import eis.iilang.Numeral;
 import eis.iilang.Parameter;
+import eis.iilang.ParameterList;
+import nl.tytech.data.engine.item.Indicator;
 import nl.tytech.data.engine.item.Stakeholder;
 
 public class J2Stakeholder implements Java2Parameter<Stakeholder> {
 
 	@Override
 	public Parameter[] translate(Stakeholder o) throws TranslationException {
-		return new Parameter[] { new Identifier(o.getName()) };
+	    if (o.getMyIndicators().isEmpty()) {
+	        return new Parameter[] { new Identifier(o.getName()) };
+	    }
+		return new Parameter[] { new Function("IndicatorLink", new Numeral(o.getID()), indicator(o.getMyIndicators(), o)) };
 	}
 
 	@Override
@@ -18,4 +27,13 @@ public class J2Stakeholder implements Java2Parameter<Stakeholder> {
 		return Stakeholder.class;
 	}
 
+	public ParameterList indicator(List<Indicator> indicatorList, Stakeholder s) {
+	    ParameterList pList = new ParameterList();
+	    for (Indicator ind : indicatorList) {
+	        pList.add(new Function("IndicatorWeights",  new Numeral(ind.getID()), 
+	                new Identifier(ind.getName()), new Numeral(s.getCurrentIndicatorWeight(ind))));
+	    }
+	    return pList;
+	}
+	
 }
