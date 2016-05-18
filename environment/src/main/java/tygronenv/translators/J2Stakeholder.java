3e0f7485bcac98878237b95766a41a29e6b4b0eb
Rico Tubbing <rico12978@hotmail.com>
2016-05-18 13:20:43 +0200

    Stakeholder percept now has a list of vars
    The list contains the ID, name, yearly income and start budget of a stakeholder

--- environment/src/main/java/tygronenv/translators/J2Stakeholder.java
+++ environment/src/main/java/tygronenv/translators/J2Stakeholder.java
@@ -3,14 +3,17 @@ package tygronenv.translators;
 import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Java2Parameter;
 import eis.iilang.Identifier;
+import eis.iilang.Numeral;
 import eis.iilang.Parameter;
+import eis.iilang.ParameterList;
 import nl.tytech.data.engine.item.Stakeholder;
 
 public class J2Stakeholder implements Java2Parameter<Stakeholder> {
 
 	@Override
 	public Parameter[] translate(Stakeholder o) throws TranslationException {
-		return new Parameter[] { new Identifier(o.getName()) };
+		return new Parameter[] { new Numeral(o.getID()), new Identifier(o.getName()),
+				new Numeral(o.getStartBudget()), new Numeral(o.getYearlyIncome())};
 	}
 
 	@Override
