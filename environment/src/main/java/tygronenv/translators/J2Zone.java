78b2a729afbfa77876a582960379783642227f1d
jannelie <jndevries@student.tudelft.nl>
2016-05-18 13:24:18 +0200

    added list of function ID's, instead of hash values

--- environment/src/main/java/tygronenv/translators/J2Zone.java
+++ environment/src/main/java/tygronenv/translators/J2Zone.java
@@ -18,8 +18,18 @@ public class J2Zone implements Java2Parameter<Zone>{
 		return new Parameter[] { new Function("Zone", new Numeral(z.getID()), new Identifier(z.getName()), new Numeral(z.getMaxAllowedFloors()) 
 				, new Numeral(size(z))
 				, translator.translate2Parameter(z.getAllowedCategories())[0]
-				//, new Numeral(FunctionID)
+				, functionId(z)
 				)};
+	}
+	public Numeral[] functionId(Zone z){
+		ArrayList<Numeral> functionid = new ArrayList<>();
+		for(Category category : z.getAllowedCategories()){
+			for (Function function : z.<Function> getMap(MapLink.FUNCTIONS).values()) {
+	            if (function.getCategories().contains(category) && !functionid.contains(new Numeral(function.getID()))) {
+	            	functionid.add(new Numeral(function.getID()))
+	            }
+	        }
+		}
 	}
 	
 	public double size(Zone z){
