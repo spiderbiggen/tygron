c79679b8b828435d2437a7738f01202b41f7d985
hmyeh <h.m.yeh@student.tudelft.nl>
2016-05-18 12:46:28 +0200

    Added J2Stakeholder test and javadoc

--- environment/src/main/java/tygronenv/translators/J2Stakeholder.java
+++ environment/src/main/java/tygronenv/translators/J2Stakeholder.java
@@ -12,21 +12,41 @@ import eis.iilang.ParameterList;
 import nl.tytech.data.engine.item.Indicator;
 import nl.tytech.data.engine.item.Stakeholder;
 
+/**
+ * Translator class from Stakeholder class into IndicatorLink Parameter.
+ * IndicatorLink(<stakeholderID>, [(<indicatorID>, <indicatorName>, 
+ * <indicatorWeight>), ..])
+ * @author Haoming
+ *
+ */
 public class J2Stakeholder implements Java2Parameter<Stakeholder> {
 
+    /**
+     * Method to translate the stakeholder into an indicatorLink percept.
+     */
 	@Override
 	public Parameter[] translate(Stakeholder o) throws TranslationException {
 	    if (o.getMyIndicators().isEmpty()) {
 	        return new Parameter[] { new Identifier(o.getName()) };
 	    }
-		return new Parameter[] { new Function("IndicatorLink", new Numeral(o.getID()), indicator(o.getMyIndicators(), o)) };
+		return new Parameter[] { new Function("IndicatorLink", new Numeral(o.getID()), 
+		        indicator(o.getMyIndicators(), o)) };
 	}
 
+	/**
+	 * Method which returns the class it translates.
+	 */
 	@Override
 	public Class<? extends Stakeholder> translatesFrom() {
 		return Stakeholder.class;
 	}
 
+	/**
+	 * Method for creating the tupled list of indicators and weights.
+	 * @param indicatorList List of indicators of the stakeholder.
+	 * @param s Stakeholder that has these indicators.
+	 * @return ParameterList with indicatorWeights.
+	 */
 	public ParameterList indicator(List<Indicator> indicatorList, Stakeholder s) {
 	    ParameterList pList = new ParameterList();
 	    for (Indicator ind : indicatorList) {
---
+++ environment/src/test/java/tygronenv/J2StakeholderTest.java
@@ -0,0 +1,58 @@
+package tygronenv;
+
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+import eis.eis2java.exception.TranslationException;
+import nl.tytech.data.engine.item.Indicator;
+import nl.tytech.data.engine.item.Stakeholder;
+import tygronenv.translators.J2Stakeholder;
+
+/**
+ * Test class for the J2Stakeholder translator
+ * @author Haoming
+ *
+ */
+public class J2StakeholderTest {
+    private J2Stakeholder translator;
+    private Stakeholder stakeholder;
+    private Indicator indicator;
+    
+    /**
+     * Initialization method called before every test.
+     */
+    @Before
+    public void init() {
+        translator = new J2Stakeholder();
+        
+        stakeholder = mock(Stakeholder.class);
+        
+        List<Indicator> list = new LinkedList<Indicator>();
+        indicator = mock(Indicator.class);
+        list.add(indicator);
+        
+        when(stakeholder.getMyIndicators()).thenReturn(list);
+        
+    }
+    
+    /**
+     * Test method which verifies that the mocked object calls the getMyIndicators method.
+     * The other methods cannot be verified, since they are final methods.
+     * That does not work with mockito.
+     * @throws TranslationException thrown if translating fails.
+     */
+    @Test
+    public void testVerifyGetMyIndicators() throws TranslationException {
+        translator.translate(stakeholder);
+        verify(stakeholder).getMyIndicators();
+        
+    }
+
+}
