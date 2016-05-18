5bc3f34546bb1f64332a4b8b3cd9ff87d54d99c2
ardwennem2 <tom_brunner@hotmail.com>
2016-05-18 13:13:08 +0200

    Revert "added hashcode"
    This reverts commit 305fdc1f1f5a690cbe23dda638ff496f18c61b6a.

--- environment/src/main/java/tygronenv/translators/J2Zone.java
+++ environment/src/main/java/tygronenv/translators/J2Zone.java
@@ -18,6 +18,7 @@ public class J2Zone implements Java2Parameter<Zone>{
 		return new Parameter[] { new Function("Zone", new Numeral(z.getID()), new Identifier(z.getName()), new Numeral(z.getMaxAllowedFloors()) 
 				, new Numeral(size(z))
 				, translator.translate2Parameter(z.getAllowedCategories())[0]
+				//, new Numeral(FunctionID)
 				)};
 	}
 	
