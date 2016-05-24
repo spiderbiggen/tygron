b052c883050fd9e255584a6cd49dfa0e7c1fd855
Frank <frank@tygron.com>
2016-05-24 10:55:47 +0200

    work in progress for popups

--- sdk-tests/src/test/java/nl/tytech/sdk/example/ExampleEventHandler.java
+++ sdk-tests/src/test/java/nl/tytech/sdk/example/ExampleEventHandler.java
@@ -1,55 +1,82 @@
 package nl.tytech.sdk.example;
 
 import java.util.Collection;
+
 import nl.tytech.core.client.event.EventIDListenerInterface;
 import nl.tytech.core.client.event.EventManager;
 import nl.tytech.core.event.Event;
 import nl.tytech.core.event.EventListenerInterface;
 import nl.tytech.core.net.serializable.MapLink;
+import nl.tytech.data.engine.item.Land;
 import nl.tytech.data.engine.item.Setting;
 import nl.tytech.data.engine.item.Stakeholder;
 import nl.tytech.util.logger.TLogger;
 
 public class ExampleEventHandler implements EventListenerInterface, EventIDListenerInterface {
 
-    private boolean stakeholderUpdate = false, mapUpdate = false;
+	private boolean stakeholderUpdate = false, mapUpdate = false;
+	private boolean landsUpdate = false, popupsUpdate = false;
 
-    public ExampleEventHandler() {
-        EventManager.addListener(this, MapLink.STAKEHOLDERS);
-        EventManager.addEnumListener(this, MapLink.SETTINGS, Setting.Type.MAP_WIDTH_METERS);
-    }
+	public ExampleEventHandler() {
+		EventManager.addListener(this, MapLink.STAKEHOLDERS, MapLink.LANDS, MapLink.POPUPS);
+		EventManager.addEnumListener(this, MapLink.SETTINGS, Setting.Type.MAP_WIDTH_METERS);
+	}
 
-    public boolean isMapUpdated() {
-        return mapUpdate;
-    }
+	public boolean isMapUpdated() {
+		return mapUpdate;
+	}
 
-    public boolean isStakeholderUpdated() {
-        return stakeholderUpdate;
-    }
+	public boolean isStakeholderUpdated() {
+		return stakeholderUpdate;
+	}
 
-    @Override
-    public void notifyEnumListener(Event event, Enum<?> enhum) {
+	public void resetLandUpdate() {
+		landsUpdate = false;
+	}
 
-        if (enhum == Setting.Type.MAP_WIDTH_METERS) {
-            Setting setting = EventManager.getItem(MapLink.SETTINGS, Setting.Type.MAP_WIDTH_METERS);
-            TLogger.info("Map Width is set to: " + setting.getIntValue());
-            mapUpdate = true;
-        }
-    }
+	public void resetPopupsUpdate() {
+		popupsUpdate = false;
+	}
 
-    @Override
-    public void notifyIDListener(Event arg0, Integer arg1) {
+	public boolean isLandUpdated() {
+		return landsUpdate;
+	}
 
-    }
+	@Override
+	public void notifyEnumListener(Event event, Enum<?> enhum) {
 
-    @Override
-    public void notifyListener(Event event) {
+		if (enhum == Setting.Type.MAP_WIDTH_METERS) {
+			Setting setting = EventManager.getItem(MapLink.SETTINGS, Setting.Type.MAP_WIDTH_METERS);
+			TLogger.info("Map Width is set to: " + setting.getIntValue());
+			mapUpdate = true;
+		}
+	}
 
-        if (event.getType() == MapLink.STAKEHOLDERS) {
-            Collection<Stakeholder> updates = event.getContent(MapLink.UPDATED_COLLECTION);
-            TLogger.info("Updated stakeholders: " + updates);
-            stakeholderUpdate = true;
-        }
-    }
+	@Override
+	public void notifyIDListener(Event arg0, Integer arg1) {
+
+	}
+
+	@Override
+	public void notifyListener(Event event) {
+
+		if (event.getType() == MapLink.STAKEHOLDERS) {
+			Collection<Stakeholder> updates = event.getContent(MapLink.UPDATED_COLLECTION);
+			TLogger.info("Updated stakeholders: " + updates);
+			stakeholderUpdate = true;
+		} else if (event.getType() == MapLink.LANDS) {
+			Collection<Land> updates = event.getContent(MapLink.UPDATED_COLLECTION);
+			TLogger.info("Updated lands: " + updates);
+			landsUpdate = true;
+		} else if (event.getType() == MapLink.POPUPS) {
+			Collection<Land> updates = event.getContent(MapLink.UPDATED_COLLECTION);
+			TLogger.info("Updated popups: " + updates);
+			popupsUpdate = true;
+		}
+	}
+
+	public boolean isPopupUpdated() {
+		return popupsUpdate;
+	}
 
 }
