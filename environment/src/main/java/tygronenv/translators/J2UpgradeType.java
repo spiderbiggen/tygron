de5945f03b375fc04728b01f7a6fe6648de46c59
Marco Houtman <m.houtman@student.tudelft.nl>
2016-05-24 22:44:19 +0200

    cleanup J2UpgradeType
    removed unused imports and expanded the Translate explanation.

--- environment/src/main/java/tygronenv/translators/J2UpgradeType.java
+++ environment/src/main/java/tygronenv/translators/J2UpgradeType.java
@@ -1,23 +1,17 @@
 package tygronenv.translators;
 
-import java.util.List;
-import java.util.Set;
-
 import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Java2Parameter;
 import eis.eis2java.translation.Translator;
 import eis.iilang.Function;
-import eis.iilang.Identifier;
 import eis.iilang.Numeral;
 import eis.iilang.Parameter;
-import eis.iilang.ParameterList;
-import nl.tytech.data.engine.item.Building;
-import nl.tytech.data.engine.item.GlobalIndicator;
 import nl.tytech.data.engine.item.UpgradeType;
 import nl.tytech.data.engine.serializable.UpgradePair;
 
 /**
- * Translate {@link UpgradeType} into UpgradeType(ID, [UpgradePair]).
+ * Translate {@link UpgradeType} into UpgradeType(UpgradeTypeID, BuildingSourceID, BuildingTargetID).
+ * UpgradeTypes without pairs are returned as UpgradeType(UpgradeTypeID).
  * 
  * @author M.Houtman
  *
@@ -29,8 +23,10 @@ public class J2UpgradeType implements Java2Parameter<UpgradeType> {
 	@Override
 	public Parameter[] translate(UpgradeType u) throws TranslationException {
 		if(u.getPairs().size()>0){
-		UpgradePair pair = u.getPairs().get(0);
-		return new Parameter[] { new Function("upgrade_type", new Numeral(u.getID()), new Numeral(pair.getSourceFunctionID()), new Numeral(pair.getTargetFunctionID())) };
+			UpgradePair pair = u.getPairs().get(0);
+			return new Parameter[] { new Function("upgrade_type", new Numeral(u.getID()), 
+					new Numeral(pair.getSourceFunctionID()), 
+					new Numeral(pair.getTargetFunctionID())) };
 		}
 		return new Parameter[] { new Function("upgrade_type", new Numeral(u.getID()))};
 	}
