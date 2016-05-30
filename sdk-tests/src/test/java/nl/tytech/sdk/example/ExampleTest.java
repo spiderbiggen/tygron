a6f995dd7739b1318af2090c23097bdb9907b7ab
Wouter1 <W.Pasman@TUDelft.nl>
2016-05-30 10:55:50 +0200

    removed unused var

--- sdk-tests/src/test/java/nl/tytech/sdk/example/ExampleTest.java
+++ sdk-tests/src/test/java/nl/tytech/sdk/example/ExampleTest.java
@@ -40,8 +40,6 @@ import nl.tytech.util.logger.TLogger;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleTest {
 
-	private final static String SERVER = "preview.tygron.com";
-
 	private static Integer slotID;
 
 	private static JoinReply reply;
