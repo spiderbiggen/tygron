6b70215534c1064ddae6ad7ae666b4037fd5680d
JoshCode <joshua.b.slik@gmail.com>
2016-05-31 22:47:35 +0200

    Fix formatting

--- environment/src/main/java/tygronenv/translators/J2Indicator.java
+++ environment/src/main/java/tygronenv/translators/J2Indicator.java
@@ -13,7 +13,6 @@ import nl.tytech.data.engine.item.Indicator;
  * Translates {@link Indicator} into indicator(ID, Current, Target).
  *
  * @author Stefan de Vringer
- *
  */
 public class J2Indicator implements Java2Parameter<Indicator> {
 
@@ -22,52 +21,54 @@ public class J2Indicator implements Java2Parameter<Indicator> {
 	 */
 	@Override
 	public Parameter[] translate(final Indicator indicator) throws TranslationException {
-	  String explanation = indicator.getExplanation();
-	  int indID = indicator.getID();
-	  double target = indicator.getTarget();
-	  double currentValue = 0;
-	  ParameterList pl = new ParameterList();
-	  if(explanation.contains("<p hidden>")){
-      explanation = indicator.getExplanation().split("<p hidden>")[1].split("</p>")[0];
-      if(explanation.contains("multi")){
-        currentValue = Double.parseDouble(explanation.split("multi")[0]);
-        pl = zoneLink(indID, target, explanation.split("multi")[1]);
-      } else if (explanation.contains("multiT")){
-        String[] targetValues = explanation.split("MultiT")[0].split("\\\\t");
-        currentValue = Double.parseDouble(targetValues[0]);
-        target = Double.parseDouble(targetValues[1]);
-        pl = zoneLink(indID, target, explanation.split("multi")[1]);
-      }
-    } 
-		return new Parameter[] {new Function("indicator",
-		    new Numeral(indID),
+		String explanation = indicator.getExplanation();
+		int indID = indicator.getID();
+		double target = indicator.getTarget();
+		double currentValue = 0;
+		ParameterList pl = new ParameterList();
+
+		if (explanation.contains("<p hidden>")) {
+			explanation = indicator.getExplanation().split("<p hidden>")[1].split("</p>")[0];
+			if (explanation.contains("multi")) {
+				currentValue = Double.parseDouble(explanation.split("multi")[0]);
+				pl = zoneLink(indID, target, explanation.split("multi")[1]);
+			} else if (explanation.contains("multiT")) {
+				String[] targetValues = explanation.split("MultiT")[0].split("\\\\t");
+				currentValue = Double.parseDouble(targetValues[0]);
+				target = Double.parseDouble(targetValues[1]);
+				pl = zoneLink(indID, target, explanation.split("multi")[1]);
+			}
+		}
+		return new Parameter[]{new Function("indicator",
+				new Numeral(indID),
 				new Numeral(currentValue),
 				new Numeral(target), pl
-		    )};
+		)};
 	}
 
 	/**
 	 * Translates a list of items into a ParameterList of zonelinks
-	 * @param id The id of the indicator.
-	 * @param target The target of the indicator.
+	 *
+	 * @param id       The id of the indicator.
+	 * @param target   The target of the indicator.
 	 * @param itemList The list of items to parse.
 	 * @return ParameterList of zoneLinks
 	 */
-  public ParameterList zoneLink(int id, double target, String itemList) {
-    ParameterList pList = new ParameterList();
-    String[] items = itemList.split("\\\\n");
-    for (String item : items) {
-      String[] types = item.split("\\\\t");
-      if(types.length==2){
-      pList.add(new Function("zone_link",  new Numeral(Integer.parseInt(types[0])), 
-          new Numeral(id), new Numeral(Double.parseDouble(types[1])), new Numeral(target)));
-      } else if(types.length==3) {
-        pList.add(new Function("zone_link",  new Numeral(Integer.parseInt(types[0])), 
-            new Numeral(id), new Numeral(Double.parseDouble(types[1])), new Numeral(Double.parseDouble(types[2]))));
-      }
-    }
-    return pList;
-  }
+	public ParameterList zoneLink(int id, double target, String itemList) {
+		ParameterList pList = new ParameterList();
+		String[] items = itemList.split("\\\\n");
+		for (String item : items) {
+			String[] types = item.split("\\\\t");
+			if (types.length == 2) {
+				pList.add(new Function("zone_link", new Numeral(Integer.parseInt(types[0])),
+						new Numeral(id), new Numeral(Double.parseDouble(types[1])), new Numeral(target)));
+			} else if (types.length == 3) {
+				pList.add(new Function("zone_link", new Numeral(Integer.parseInt(types[0])),
+						new Numeral(id), new Numeral(Double.parseDouble(types[1])), new Numeral(Double.parseDouble(types[2]))));
+			}
+		}
+		return pList;
+	}
 
 	/**
 	 * Get the class which is translated from.
