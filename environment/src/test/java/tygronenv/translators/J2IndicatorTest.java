9bf57ed9e709a78745fb566d1ddeaedaa668e119
JoshCode <joshua.b.slik@gmail.com>
2016-05-31 23:43:35 +0200

    Fix typo in word translator

--- environment/src/test/java/tygronenv/translators/J2IndicatorTest.java
+++ environment/src/test/java/tygronenv/translators/J2IndicatorTest.java
@@ -43,7 +43,7 @@ public class J2IndicatorTest {
      * @throws TranslationException thrown if the translate method fails.
      */
     @Test
-    public void tranlatorTest1() throws TranslationException {
+    public void translatorTest1() throws TranslationException {
         when(indicator.getExplanation()).thenReturn("<p hidden>0\\t1\\n1\\t1\\n2\\t1\\n3\\t1\\n4\\t1\\n5\\t1\\n6\\t1\\n7\\t1\\n8\\t1\\n9\\t1\\n10\\t1\\n11\\t1\\n12\\t1\\n</p>");
         translator.translate(indicator);
         verify(indicator, times(1)).getExactNumberValue(MapType.MAQUETTE);
