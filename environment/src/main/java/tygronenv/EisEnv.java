b30977b448096b6a6397c94aafa8ea4ba79f96e5
Stefan Breetveld <spiderbiggen@gmail.com>
2016-06-01 17:45:16 +0200

    enable multiple upgrade_pairs in upgrade_type

--- environment/src/main/java/tygronenv/EisEnv.java
+++ environment/src/main/java/tygronenv/EisEnv.java
@@ -16,26 +16,8 @@ import eis.iilang.Parameter;
 import eis.iilang.Percept;
 import tygronenv.configuration.Configuration;
 import tygronenv.connection.ServerConnection;
-import tygronenv.translators.HashMap2J;
-import tygronenv.translators.J2ActionLog;
-import tygronenv.translators.J2ActionMenu;
-import tygronenv.translators.J2Answer;
-import tygronenv.translators.J2Building;
-import tygronenv.translators.J2Category;
-import tygronenv.translators.J2ClientItemMap;
-import tygronenv.translators.J2Function;
-import tygronenv.translators.J2Indicator;
-import tygronenv.translators.J2Land;
-import tygronenv.translators.J2MultiPolygon;
-import tygronenv.translators.J2PopupData;
-import tygronenv.translators.J2Setting;
-import tygronenv.translators.J2Stakeholder;
-import tygronenv.translators.J2TimeState;
-import tygronenv.translators.J2UpgradeType;
-import tygronenv.translators.J2Zone;
-import tygronenv.translators.MultiPolygon2J;
-import tygronenv.translators.ParamEnum2J;
-import tygronenv.translators.Stakeholder2J;
+import tygronenv.translators.*;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -171,7 +153,7 @@ public class EisEnv extends EIDefaultImpl {
 	private Java2Parameter<?>[] j2p = new Java2Parameter<?>[]{new J2ClientItemMap(), new J2Stakeholder(),
 	    new J2Setting(), new J2Function(), new J2Category(), new J2Building(), new J2TimeState(),
 	    new J2ActionLog(), new J2ActionMenu(), new J2Zone(), new J2Land(), new J2MultiPolygon(),
-	    new J2PopupData(), new J2Answer(), new J2Indicator(), new J2UpgradeType()};
+	    new J2PopupData(), new J2Answer(), new J2Indicator(), new J2UpgradeType(), new J2UpgradePair()};
 
 	private Parameter2Java<?>[] p2j = new Parameter2Java<?>[]{new ParamEnum2J(),
 	  new HashMap2J(), new Stakeholder2J(), new MultiPolygon2J()};
