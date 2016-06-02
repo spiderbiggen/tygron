33904cf926d60828c499aecc961c1db1921ca88c
Rico Tubbing <rico12978@hotmail.com>
2016-06-02 14:46:11 +0200

    Fix groundlayerType2j, with checkstyle and a bug
    [ci skip]

--- environment/src/main/java/tygronenv/translators/GroundLayerType2J.java
+++ environment/src/main/java/tygronenv/translators/GroundLayerType2J.java
@@ -6,16 +6,29 @@ import eis.iilang.Identifier;
 import eis.iilang.Parameter;
 import nl.tytech.data.engine.item.Building.GroundLayerType;
 
+/**
+ * Translates a GroundLayerType argument to a java.
+ * @author Rico
+ *
+ */
 public class GroundLayerType2J implements Parameter2Java<GroundLayerType> {
 
+	private static final String SURFACE_IDENTIFIER = "SURFACE";
+	private static final String UNDERGROUND_IDENTIFIER = "UNDERGROUND";
 	@Override
-	public GroundLayerType translate(Parameter parameter) throws TranslationException {
-		Identifier name = (Identifier) parameter;
-		if (name.equals("SURFACE")) {
+
+	public GroundLayerType translate(final Parameter parameter) throws TranslationException {
+		String name =  ((Identifier) parameter).getValue();
+		name = name.toUpperCase();
+
+		if (name.equals(SURFACE_IDENTIFIER)) {
 			return GroundLayerType.SURFACE;
-		} else {
+		} else if (name.equals(UNDERGROUND_IDENTIFIER)) {
 			return GroundLayerType.UNDERGROUND;
 		}
+		throw new TranslationException("Undefined identifier found " + name
+				+ ", should be " + SURFACE_IDENTIFIER
+				+ " or " + UNDERGROUND_IDENTIFIER);
 	}
 
 	@Override
