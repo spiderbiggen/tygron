d6cd07f8cb831447fb99840187f598be6baee4da
Max <Max_Groenenboom@hotmail.com>
2016-06-03 13:25:30 +0200

    Added a method to convert triangles to new polygons.

--- environment/src/main/java/tygronenv/actions/GetRelevantAreas.java
+++ environment/src/main/java/tygronenv/actions/GetRelevantAreas.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.prep.PreparedGeometry;
@@ -146,14 +148,15 @@ public class GetRelevantAreas implements CustomAction {
 	 */
 	private List<PolygonItem> getBuildableArea(final Stakeholder stakeholder) {
 		debug("function called");
+		debug("fetching data");
 		// Fetch all necessary data from the SDK.
 		final Integer stakeholderID = stakeholder.getID();
 		final ItemMap<Land> lands = EventManager.getItemMap(MapLink.LANDS);
 		final ItemMap<Terrain> terrains = EventManager.getItemMap(MapLink.TERRAINS);
 		final Setting reservedLandSetting = EventManager.getItem(MapLink.SETTINGS, Setting.Type.RESERVED_LAND);
 		final ItemMap<Building> buildings = EventManager.getItemMap(MapLink.BUILDINGS);
-		debug("fetched data");
 
+		debug("combining land");
 		// Get a MultiPolygon of all lands combined.
 		MultiPolygon constructableLand = JTSUtils.EMPTY;
 		for (Land land : lands) {
@@ -161,23 +164,23 @@ public class GetRelevantAreas implements CustomAction {
 				constructableLand = JTSUtils.union(constructableLand, land.getMultiPolygon());
 			}
 		}
-		debug("combined land");
 
+		debug("removing water");
 		// Remove all pieces of land that cannot be build on (water).
 		for (Terrain terrain : terrains) {
 			if (terrain.getType().isWater()) {
 				constructableLand = JTSUtils.difference(constructableLand, terrain.getMultiPolygon(DEFAULT_MAPTYPE));
 			}
 		}
-		debug("removed water");
 
+		debug("removing reserved");
 		// Remove all pieces of reserved land.
 		MultiPolygon reservedLand = reservedLandSetting.getMultiPolygon();
 		if (JTSUtils.containsData(reservedLand)) {
 			constructableLand = JTSUtils.difference(constructableLand, reservedLand);
 		}
-		debug("removed reserved");
 
+		debug("removing buildings");
 		// Remove all pieces of occupied land.
 		PreparedGeometry prepped = PreparedGeometryFactory.prepare(constructableLand);
 		for (Building building : buildings) {
@@ -186,34 +189,141 @@ public class GetRelevantAreas implements CustomAction {
 				constructableLand = JTSUtils.difference(constructableLand, buildingMP);
 			}
 		}
-		debug("removed buildings");
 
+		debug("finalizing selection");
 		final int minArea = 200;
-		final int maxArea = 500;
-		final int maxPolys = 5;
+		final int maxArea = 2000;
+		final int maxPolys = 15;
 		int numPolys = 0;
 		List<PolygonItem> polygons = new LinkedList<PolygonItem>();
 		for (Polygon poly: JTSUtils.getPolygons(constructableLand)) {
 			List<Polygon> listPolygon = JTSUtils.getTriangles(poly, minArea);
-			for (Polygon triangle : listPolygon) {
+			for (Geometry geom : listPolygon) {
 				if (numPolys > maxPolys) {
 					break;
 				}
-				if (triangle.getArea() < minArea || triangle.getArea() > maxArea) {
+				debug("Before: " + geom.getArea());
+				geom = createNewPolygon(geom);
+				geom = geom.intersection(constructableLand);
+				debug("After: " + geom.getArea());
+				geom = geom.buffer(-15);
+				geom = geom.buffer(10);
+				if (geom.getArea() < minArea / 2 || geom.getArea() > maxArea) {
 					continue;
 				}
-				MultiPolygon mp = JTSUtils.createMP(triangle);
+				MultiPolygon mp = JTSUtils.createMP(geom);
 				polygons.add(new PolygonWrapper(mp));
 				numPolys++;
+				debug("Added " + mp.toText());
 			}
 		}
-
 
 		debug("created result");
 		return polygons;
 	}
 
+	/**
+	 * Create a new polygon from a triangle, by creating two new corner points.
+	 * @param triangle The triangle to derive the square from.
+	 * @return The new polygon.
+	 */
+	private Geometry createNewPolygon(Geometry triangle) {
+		if (triangle.getNumPoints() != 4) {
+			return triangle;
+		} else {
+			Coordinate[] coords = triangle.getCoordinates();
+
+			// Create first new point.
+			Coordinate newPoint1 = plus(coords[0], coords[2]);
+			newPoint1 = divide(newPoint1, 2);
+			newPoint1 = minus(newPoint1, coords[1]);
+			newPoint1 = plus(coords[1], times(newPoint1, 1.25));
+
+			// Create second new point.
+			Coordinate newPoint2 = plus(coords[0], coords[1]);
+			newPoint2 = divide(newPoint2, 2);
+			newPoint2 = minus(newPoint2, coords[2]);
+			newPoint2 = plus(coords[2], times(newPoint2, 1.25));
+
+			// Insert two new points 
+			Coordinate[] newCoords = {
+					coords[0],
+					newPoint1,
+					coords[1],
+					coords[2],
+					newPoint2,
+					coords[0]
+			};
+			
+			// Using stringbuilder and translator because no other way has been found
+			// to create multipolygons yet.
+			StringBuilder sb = new StringBuilder("MULTIPOLYGON (((");
+			for (Coordinate coord : newCoords) {
+				sb.append(coord.x + " " + coord.y + ", ");
+			}
+			sb.delete(sb.length() - 2, sb.length());
+			sb.append(")))");
+
+			MultiPolygon result = null;
+			try {
+				result = TRANSLATOR.translate2Java(new Function("multipolygon",
+						new Identifier(sb.toString())), MultiPolygon.class);
+			} catch (TranslationException e) {
+				TLogger.exception(e);
+				return triangle;
+			}
+
+			return result;
+		}
+	}
+
+	/**
+	 * Add two Coordinates together.
+	 * @param c1 First coordinate.
+	 * @param c2 Second coordinate
+	 * @return A new Coordinate.
+	 */
+	private Coordinate plus(Coordinate c1, Coordinate c2) {
+		return new Coordinate(c1.x + c2.x, c1.y + c2.y, c1.z + c2.z);
+	}
+
+	/**
+	 * Subtract one Coordinate from another.
+	 * @param c1 First coordinate.
+	 * @param c2 Second coordinate
+	 * @return A new Coordinate.
+	 */
+	private Coordinate minus(Coordinate c1, Coordinate c2) {
+		return new Coordinate(c1.x - c2.x, c1.y - c2.y, c1.z - c2.z);
+	}
+
+	/**
+	 * Multiply a Coordinate with a number.
+	 * @param c1 The coordinate.
+	 * @param num the number.
+	 * @return A new Coordinate.
+	 */
+	private Coordinate times(Coordinate c, double num) {
+		return new Coordinate(c.x * num, c.y * num, c.z * num);
+	}
+
+	/**
+	 * Divide a Coordinate with a number.
+	 * @param c1 The coordinate.
+	 * @param num the number.
+	 * @return A new Coordinate.
+	 */
+	private Coordinate divide(Coordinate c, double num) {
+		return new Coordinate(c.x / num, c.y / num, c.z / num);
+	}
+
+	/**
+	 * Print a debug message.
+	 * Is a separate function to make it easier to remove debug messages.
+	 * @param message The message.
+	 */
 	private void debug(String message) {
+		// Debug temporarliy disabled;
 		// System.out.println("Debug: " + message);
 	}
 
