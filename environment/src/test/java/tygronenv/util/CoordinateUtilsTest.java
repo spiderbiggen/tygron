e5639d411380fb8802499539d1ab2c32de03e73e
Max <Max_Groenenboom@hotmail.com>
2016-06-06 13:07:40 +0200

    Fixed checkstyle for CoordinateUtilsTest.java
    [ci skip]

--- environment/src/test/java/tygronenv/util/CoordinateUtilsTest.java
+++ environment/src/test/java/tygronenv/util/CoordinateUtilsTest.java
@@ -6,41 +6,52 @@ import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+/**
+ * Tests usage of CoordinateUtils.
+ * @author Rico Tubbing
+ */
 public class CoordinateUtilsTest {
 
-	Coordinate coordinate1 = new Coordinate(5, 10, 0);
-	Coordinate coordinate2 = new Coordinate(8, 2, 5);
+	private final Coordinate coordinate1 = new Coordinate(5, 10, 0);
+	private final Coordinate coordinate2 = new Coordinate(8, 2, 5);
 
+	/**
+	 * Tests the plus method.
+	 */
 	@Test
 	public void testPlus() {
-		Coordinate coord = CoordinateUtils.plus(coordinate1, coordinate2);
-		assertEquals(13, coord.x, 0);
-		assertEquals(12, coord.y, 0);
-		assertEquals(5, coord.z, 0);
+		final Coordinate coord = CoordinateUtils.plus(coordinate1, coordinate2);
+		final Coordinate expected = new Coordinate(13, 12, 5);
+		assertEquals(coord, expected);
 	}
 
+	/**
+	 * Tests the minus method.
+	 */
 	@Test
 	public void testMinus() {
-		Coordinate coord = CoordinateUtils.minus(coordinate1, coordinate2);
-		assertEquals(-3, coord.x, 0);
-		assertEquals(8, coord.y, 0);
-		assertEquals(-5, coord.z, 0);
+		final Coordinate coord = CoordinateUtils.minus(coordinate1, coordinate2);
+		final Coordinate expected = new Coordinate(-3, 8, -5);
+		assertEquals(coord, expected);
 	}
 
+	/**
+	 * Tests the times method.
+	 */
 	@Test
 	public void testTimes() {
-		Coordinate coord = CoordinateUtils.times(coordinate1, 4);
-		assertEquals(20, coord.x, 0);
-		assertEquals(40, coord.y, 0);
-		assertEquals(0, coord.z, 0);
+		final Coordinate coord = CoordinateUtils.times(coordinate1, 2);
+		final Coordinate expected = new Coordinate(10, 20, 0);
+		assertEquals(coord, expected);
 	}
 
+	/**
+	 * Tests the divide method.
+	 */
 	@Test
 	public void testDivide() {
-		Coordinate coord = CoordinateUtils.divide(coordinate1, 4);
-		assertEquals(1.25, coord.x, 0);
-		assertEquals(2.5, coord.y, 0);
-		assertEquals(0, coord.z, 0);
-	}	
-
+		final Coordinate coord = CoordinateUtils.divide(coordinate1, 2);
+		final Coordinate expected = new Coordinate(2.5, 5, 0);
+		assertEquals(coord, expected);
+	}
 }
