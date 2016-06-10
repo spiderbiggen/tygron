8866334ecdaa88b88b7b7747157170f19c0f67de
Stefan Breetveld <spiderbiggen@gmail.com>
2016-06-10 12:08:39 +0200

    Revert "change spacing characters from spaces to tabs"
    This reverts commit 0d923e072e3d0b7162d20ab9a0ee768f347ecec2.

--- contextvh/src/main/java/contextvh/translators/J2UpgradePair.java
+++ contextvh/src/main/java/contextvh/translators/J2UpgradePair.java
@@ -15,32 +15,31 @@ import nl.tytech.data.engine.serializable.UpgradePair;
  */
 public class J2UpgradePair implements Java2Parameter<UpgradePair> {
 
-	private final Translator translator = Translator.getInstance();
+    private final Translator translator = Translator.getInstance();
 
-	/**
-	 * Translates the UpgradeType object in the form of:
-	 * {@code upgrade_type(<ID>, <SourceFunctionID>, <TargetFunctionID>)} or
-	 * {@code upgrade_type(<ID>)} if there is no pair available.
-	 */
-	@Override
-	public Parameter[] translate(final UpgradePair upgradePair) throws TranslationException {
-		return new Parameter[] {
-				new Function(
-						"upgrade_pair",
-						new Numeral(upgradePair.getSourceFunctionID()),
-						new Numeral(upgradePair.getTargetFunctionID())
-				)
-		};
-	}
+    /**
+     * Translates the UpgradeType object in the form of:
+     * {@code upgrade_type(<ID>, <SourceFunctionID>, <TargetFunctionID>)} or
+     * {@code upgrade_type(<ID>)} if there is no pair available.
+     */
+    @Override
+    public Parameter[] translate(final UpgradePair upgradePair) throws TranslationException {
+        return new Parameter[]{
+                new Function(
+                        "upgrade_pair",
+                        new Numeral(upgradePair.getSourceFunctionID()),
+                        new Numeral(upgradePair.getTargetFunctionID())
+                )
+        };
+    }
 
-	/**
-	 * This class translates all objects that extend {@link UpgradePair}.
-	 *
-	 * @return {@code UpgradePair.class}
-	 */
-	@Override
-	public Class<? extends UpgradePair> translatesFrom() {
-		return UpgradePair.class;
-	}
+    /**
+     * This class translates all objects that extend {@link UpgradePair}.
+     * @return {@code UpgradePair.class}
+     */
+    @Override
+    public Class<? extends UpgradePair> translatesFrom() {
+        return UpgradePair.class;
+    }
 
 }
