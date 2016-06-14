d6168a07bca2d1a77865bab3228e01605ce74f5b
diony <diony@zeelandnet.nl>
2016-06-14 13:04:27 +0200

    added parameters to the get_relevant_area_build action
    amount(int)				// 15 if unspecified
    area(double, double)	//(200, 2000) if unspecified
    stakeholder(int)		// caller if unspecified
    zones(id, id , ...)		// all if unspecified

--- contextvh/src/main/java/contextvh/actions/GetRelevantAreasBuild.java
+++ contextvh/src/main/java/contextvh/actions/GetRelevantAreasBuild.java
@@ -1,6 +1,7 @@
 package contextvh.actions;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import contextvh.util.CoordinateUtils;
 import contextvh.util.MapUtils;
 import eis.eis2java.exception.TranslationException;
 import eis.iilang.Identifier;
+import eis.iilang.Numeral;
 import eis.iilang.Parameter;
 import eis.iilang.ParameterList;
 import eis.iilang.Percept;
@@ -58,11 +60,20 @@ public class GetRelevantAreasBuild implements RelevantAreasAction {
 	public void internalCall(final Percept createdPercept,
 			final ContextEntity caller, final Parameters parameters) {
 		MultiPolygon constructableLand = getUsableArea(caller, parameters);
-
-		// This should really be added as an "after-effect" instead of compulsory code.
-		final int minArea = 200, maxArea = 2000;
-		final int maxPolys = 15;
+		// Parse parameters
+		final double minArea = parameters.containsKey("area")
+				?	((Numeral)parameters.get("area").getFirst()).getValue().doubleValue()
+				:	200;
+		final double maxArea = parameters.containsKey("area")
+				?	((Numeral)parameters.get("area").getLast()).getValue().doubleValue()
+				:	2000;
+		if(minArea>=maxArea)
+			throw new IllegalArgumentException("min area must be smaller than max area");
+		final int maxPolys = parameters.containsKey("amount")
+				?	((Numeral)parameters.get("amount").getFirst()).getValue().intValue()
+				:	15;
 		final int bufferUp = 5, bufferDown = -10;
+		// Calculate polygons
 		int numPolys = 0;
 		final ParameterList results = new ParameterList();
 		for (Polygon poly: JTSUtils.getPolygons(constructableLand)) {
@@ -100,10 +111,23 @@ public class GetRelevantAreasBuild implements RelevantAreasAction {
 	protected MultiPolygon getUsableArea(final ContextEntity caller, final Parameters parameters) {
 		// Get a MultiPolygon of all lands combined.
 		Integer connectionID = caller.getSlotConnection().getConnectionID();
-
-		final Integer stakeholderID = caller.getStakeholder().getID();
+		// Get the stakeholder form the parameters or default to caller
+		final Integer stakeholderID = parameters.containsKey("stakeholder")
+				?	((Numeral)parameters.get("stakeholder").getFirst()).getValue().intValue()
+				:	caller.getStakeholder().getID();
+		// Calculate multipolygon
 		MultiPolygon constructableLand = MapUtils.getStakeholderLands(connectionID, stakeholderID);
-
+		
+		// Intersect with zones if there is a zone parameter
+		if(parameters.containsKey("zones")){
+			Iterator<Parameter> ZoneParam = parameters.get("zones").iterator();
+			List<Integer> Zones = new LinkedList<Integer>();
+			while(ZoneParam.hasNext()){
+				Zones.add((int)((Numeral)ZoneParam.next()).getValue());
+			}
+			constructableLand = (MultiPolygon) constructableLand.intersection(
+					MapUtils.getZonesCombined(connectionID, Zones));
+		}
 		// Remove all pieces of land that cannot be build on (water).
 		constructableLand = MapUtils.removeWater(connectionID, constructableLand);
 
